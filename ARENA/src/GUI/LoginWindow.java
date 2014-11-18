18b6591491211337f6c94a7effe19f02a612aa68
Simon <simonlundquist@live.com>
2014-11-18 16:20:18 +0100

    asd

--- ARENA/src/GUI/LoginWindow.java
+++ ARENA/src/GUI/LoginWindow.java
@@ -1,15 +1,18 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*;
+import javax.swing.*;
 
 public class LoginWindow extends JFrame{
 	private JPanel container;
+	private JTextField username;
 	
 	public void LoginWindow() {
+		container = new JPanel(new BorderLayout());
+		username = new JTextField();
+		container.add(username);
+		add(container);
 		setVisible(true);
 		setSize(300, 300);
-		container = new JPanel(new BorderLayout());
 	}
 }
