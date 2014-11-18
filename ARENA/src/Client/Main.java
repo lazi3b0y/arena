acba89fd1656ba45af450b1345116604e69caaf5
christoffer <christoffer@Vonsn0rk>
2014-11-18 18:52:36 +0100

    aaa

--- ARENA/src/Client/Main.java
+++ ARENA/src/Client/Main.java
@@ -8,5 +8,6 @@ public class Main {
 	public static void main(String[] args) {
 		LoginWindow login = new LoginWindow();
 		MainFrame arena = new MainFrame();
+		GamesPanel games = new GamesPanel();
 	}
 }
