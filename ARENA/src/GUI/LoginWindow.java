6acdea49dd7df7900332847a568c709731329899
Simon <simonlundquist@live.com>
2014-11-18 18:02:33 +0100

    Loginwindow pretty much färdig

--- ARENA/src/GUI/LoginWindow.java
+++ ARENA/src/GUI/LoginWindow.java
@@ -1,38 +1,71 @@
 package GUI;
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginWindow extends JFrame{
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginWindow extends JFrame implements ActionListener{
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPanel username;
 	private JPanel password;
 	private JLabel usernameLabel;
 	private JLabel passwordLabel;
 	private JTextField usernameField;
-	private JTextField passwordField;
+	private JPasswordField passwordField;
+	private JButton login;
+	private GridBagConstraints cons;
+	
+	private ActionEvent e;
 	
 	public LoginWindow() {
+		super("Login");
 		contentPane = new JPanel(new BorderLayout());
+		cons = new GridBagConstraints();
 		usernameField = new JTextField(15);
-		passwordField = new JTextField(15);
+		passwordField = new JPasswordField(15);
 		
 		username = new JPanel();
 		usernameLabel = new JLabel("Username");
 		username.add(usernameLabel);
 		username.add(usernameField);
+		contentPane.add(username, BorderLayout.NORTH);
 		
 		password = new JPanel();
 		passwordLabel = new JLabel("Password");
 		password.add(passwordLabel);
 		password.add(passwordField);
+		contentPane.add(password, BorderLayout.CENTER);
 		
-		contentPane.add(username, BorderLayout.NORTH);
-		contentPane.add(password, BorderLayout.SOUTH);
+		login = new JButton("Login");
+		login.addActionListener(this);
+		contentPane.add(login, BorderLayout.SOUTH);
 		contentPane.setBorder(BorderFactory.createEmptyBorder(15, 10, 10, 10));
 		add(contentPane);
 		setVisible(true);
-		setSize(300, 115);
+		setSize(300, 150);
 		setResizable(false);
+		//setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		String username;
+		char[] password;
+		username = usernameField.getText();
+		password = passwordField.getPassword();
+		System.out.println(username + " ");
+		for(int i = 0; i < password.length; i++) {
+			System.out.print(password[i]);
+		}
 	}
 }
