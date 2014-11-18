f81af81cddaff3a6203ade777bdacc57c52cd1e7
Nicklas <Nicklas@Nicklas-Dator.lan>
2014-11-19 00:04:45 +0100

    Some comments

--- ARENA/src/technical/database/Database.java
+++ ARENA/src/technical/database/Database.java
@@ -13,33 +13,41 @@ public abstract class Database {
 	// For security purposes
 	private char[] password;
 
-	public Database(String driver, String dbName, int port, String userName,
-			char[] password) {
-		this.driver = driver;
+	public Database(String driver, String dbName, int port, String userName, char[] password) {
+		this.driver   = driver;
 		this.userName = userName;
 		this.password = password;
-		this.port = port;
-		this.dbName = dbName;
+		this.port 	  = port;
+		this.dbName	  = dbName;
 	}
 
 	
+	/**
+	   * Connect to the database. Every sub class must explicitly call this method.
+	   */ 
+	
 	protected void connect() {
 		try {
+			// In order for the driver to be loaded at runtime, add the appropriate driver paths in the class path using either the IDE or command prompt.
 			Class.forName(driver);
 
-			setConnection(DriverManager.getConnection(getURL(), userName,
-					password.toString()));
-			System.out.println("hej2");
+			this.connection = DriverManager.getConnection(getURL(), userName, password.toString());
+			
 		} catch (ClassNotFoundException classEx) {
 			classEx.printStackTrace();
+			
 		} catch (SQLException sqlEx) {
 			sqlEx.printStackTrace();
+			
 		} finally {
 			password = null;
 
 		}
 	}
 
+	/**
+	   * Disconnect 
+	   */ 
 	public void disconnect() {
 		try {
 			connection.close();
@@ -76,23 +84,44 @@ public abstract class Database {
 		}
 	}
 
-	public ResultSet executeQuery(String sqlCommand, SqlParameter... params)
-			throws SQLException {
+	/**
+	   * Executes a query that expects a result set (such as SELECT, etc). It will automatically prepare statements, so SQL commands need to be adapted to this.
+	   * For instance, the value in the WHERE clause in SELECT query should be replaced with ?, and the value(s) should be passed as SqlParameter objects to this method. 
+	   * @param sqlCommand 
+	   * @param SqlParameter An array of SqlParameter objects
+	   * @exception throws SQLException
+	   * @return A SQL result set
+	   */ 
+	public ResultSet executeQuery(String sqlCommand, SqlParameter... params) throws SQLException {
 		PreparedStatement statement = createPreparedStatement(sqlCommand,
 				params);
 		ResultSet rs = statement.executeQuery();
 		return rs;
 	}
+	
+	/**
+	   * Executes a SQL query such as UPDATE or INSERT, etc.
+	   * @param sqlCommand 
+	   * @param SqlParameter An array of SqlParameter objects
+	   * @exception throws SQLException
+	   * @return void
+	   */ 
 
-	public void executeNonQuery(String sqlCommand, SqlParameter... params)
-			throws SQLException {
+	public void executeNonQuery(String sqlCommand, SqlParameter... params) throws SQLException {
 		PreparedStatement statement = createPreparedStatement(sqlCommand,
 				params);
 		statement.executeUpdate();
 	}
 
-	private PreparedStatement createPreparedStatement(String sqlCommand,
-			SqlParameter... params) throws SQLException {
+	/**
+	   * Prepares a SQL statement
+	   * @param sqlCommand 
+	   * @param SqlParameter An array of SqlParameter objects
+	   * @exception throws SQLException
+	   * @exception throws SQLException
+	   * @return Prepared statement
+	   */ 
+	private PreparedStatement createPreparedStatement(String sqlCommand, SqlParameter... params) throws SQLException {
 
 		PreparedStatement statement = connection.prepareStatement(sqlCommand);
 
@@ -114,10 +143,6 @@ public abstract class Database {
 
 	public Connection getConnection() {
 		return connection;
-	}
-
-	private void setConnection(Connection conn) {
-		this.connection = conn;
 	}
 
 	public String getUserName() {
