61bd8c0b34213df8a26f137085937711c48b5970
woppaway <oskar_000@LillaSyd.lan>
2014-11-18 18:40:00 +0100

    Pill med MainFrame

--- ARENA/src/GUI/MainFrame.java
+++ ARENA/src/GUI/MainFrame.java
@@ -1,20 +1,26 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+
+import javax.swing.*;
+
 
 public class MainFrame extends JFrame{ 
+	private static final long serialVersionUID = 1L;
+	private ImageIcon image = new ImageIcon(getClass().getResource("ThumbsUp.jpg"));
+	GridBagConstraints cons = new GridBagConstraints();
+	JLabel newsLabel = new JLabel(image);
 	JPanel contentPane = new JPanel(new BorderLayout());
 	JPanel topBar = new JPanel();
+	JPanel newsBar = new JPanel();
 	JButton Games = new JButton("Games");
 	JButton News = new JButton("News");
 	JButton Profile = new JButton("Profile");
 	JButton LogOut = new JButton("Log Out");
-	
-	private static final long serialVersionUID = 1L;
 
 	public MainFrame() {
 		super("ARENA");
@@ -24,8 +30,14 @@ public class MainFrame extends JFrame{
 		topBar.add(Profile);
 		topBar.add(LogOut);
 		contentPane.add(topBar, BorderLayout.NORTH);
+		newsBar.setLayout(new GridBagLayout());
+		cons.fill = GridBagConstraints.VERTICAL;
+		cons.gridx = -1;
+		cons.gridy = 0;
+		newsBar.add(newsLabel, cons);
+		contentPane.add(newsBar, BorderLayout.WEST);
 		add(contentPane);
-		setSize(800, 600);
+		setSize(800, 500);
 		setResizable(false);
 		setVisible(true);
 		setLocationRelativeTo(null);
