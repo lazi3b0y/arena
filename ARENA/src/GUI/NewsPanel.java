a98240f791eef3f8c0c471ca9f0d27fdf7ae0e68
woppaway <oskar.soder@hotmail.com>
2014-11-21 11:51:16 +0100

    Lite GUI krafs

--- ARENA/src/GUI/NewsPanel.java
+++ ARENA/src/GUI/NewsPanel.java
@@ -4,12 +4,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class NewsPanel extends JPanel{
-	JLabel text;
+	JLabel headline, news1, news2, news3;
 	
 	public NewsPanel() {
 		setLayout(null);
-		text = new JLabel("Nyheter");
-		text.setBounds(0,0,200,15);
-		add(text);
+		headline = new JLabel("Incomming updates:");
+		headline.setBounds(20,20,200,25);
+		add(headline);
+		
+		news1 = new JLabel("-Ingame shop coming soon to ARENA");
+		news1.setBounds(20, 45, 350, 25);
+		add(news1);
+		
+		news2 = new JLabel("-FourInRow coming soon to ARENA");
+		news2.setBounds(20, 70, 350, 25);
+		add(news2);
+		
+		news3 = new JLabel("-Sign up now for the upcoming FourInRow tournament. Starts November 27. Ends November 30.");
+		news3.setBounds(20, 95, 550, 25);
+		add(news3);
+		
+		
+		
 	}
 }
--- ARENA/src/GUI/ProfilePanel.java
+++ ARENA/src/GUI/ProfilePanel.java
@@ -4,13 +4,35 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ProfilePanel extends JPanel{
-	JLabel text;
+	JLabel profile, nickname, wins, losses, winLoseRatio, ranking;
 	
 	public ProfilePanel(){
 		setLayout(null);
-		text = new JLabel("Profil");
-		text.setBounds(0,0,200,15);
-		add(text);
+		profile = new JLabel("Profile:");
+		profile.setBounds(20,20,200,15);
+		add(profile);
+		
+		nickname = new JLabel("Nickname: BertiL^"/*nickname.GetNickname();*/);
+		nickname.setBounds(20,35,150,15);
+		add(nickname);
+		
+		ranking = new JLabel("Ranking: BästIVerden"/*ranking.GetRank();*/);
+		ranking.setBounds(20, 50, 150, 15);
+		add(ranking);
+		
+		wins = new JLabel("Wins: 13"/*wins.GetWins();*/);
+		wins.setBounds(20, 65, 100, 15);
+		add(wins);
+		
+		losses = new JLabel("Losses: 13"/*losses.GetLosses();*/);
+		losses.setBounds(20, 80, 100, 15);
+		add(losses);
+		
+		winLoseRatio = new JLabel("W/L-Ratio: 0"/*winLoseRatio.GetWLRatio();*/);
+		winLoseRatio.setBounds(20, 95, 100, 15);
+		add(winLoseRatio);
+		
+		
 	}
 	
 	
