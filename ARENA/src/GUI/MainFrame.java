b29bfed13e66ffdba6847f49b06a31bac3897b85
Nicklas <Nicklas@Nicklas-Dator.lan>
2014-11-21 23:18:04 +0100

    Fixed confirm dialog on logging out

--- ARENA/src/GUI/MainFrame.java
+++ ARENA/src/GUI/MainFrame.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,8 +10,10 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+
 import GUI.gamepanel.*;
 import GUI.menu.AdminMenu;
 import GUI.menu.UsersMenu;
@@ -18,7 +21,7 @@ import GUI.menu.UsersMenu;
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private static final int windowWidth = 800, windowHeight = 528;
-	
+
 	private ImageIcon ad;
 	private JButton gamesBtn, newsBtn, profileBtn, logOutBtn;
 	private JLabel adContainer;
@@ -41,46 +44,46 @@ public class MainFrame extends JFrame {
 		profilePanel = new ProfilePanel();
 		menuBar = new AdminMenu();
 		Choice = true;
-		
+
 		setLayout(null);
 		setSize(windowWidth, windowHeight);
 		setResizable(false);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
 		gamesBtn.setBounds(0, 20, 200, 30);
 		add(gamesBtn);
-		
+
 		newsBtn.setBounds(200, 20, 200, 30);
 		add(newsBtn);
-		
+
 		profileBtn.setBounds(400, 20, 200, 30);
 		add(profileBtn);
-		
+
 		logOutBtn.setBounds(600, 20, 200, 30);
 		add(logOutBtn);
-		
+
 		adContainer.setBounds(600, 50, 200, 450);
 		add(adContainer);
-		
+
 		newsPanel.setBounds(0, 50, 600, 450);
 		profilePanel.setBounds(0, 50, 600, 450);
 		gamesPanel.setBounds(0, 50, 600, 450);
 		add(gamesPanel);
-		
+
 		menuBar.setBounds(0, 0, windowWidth, 20);
 		add(menuBar);
-		
+
 		InitActionListeners();
 	}
-	
-	public void InitActionListeners(){
-		
+
+	public void InitActionListeners() {
+
 		/**
 		 * JButton ActionListeners
 		 */
-		
+
 		gamesBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				remove(newsPanel);
@@ -110,25 +113,23 @@ public class MainFrame extends JFrame {
 		});
 		logOutBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//FIX ME
-				//Dialog som frï¿½gar om man ï¿½r sï¿½ker pï¿½ att man vill logga ut. 
-				//Om användaren väljer Ja så ska huvudfönstret stängas ner och login rutan visas på nytt.
-				LogoutWindow logoutFrame = new LogoutWindow(Choice);
-				if(Choice == true){
+				int dialogRes = JOptionPane.showConfirmDialog(null, "Are you sured you want to log out?",  "Log out", JOptionPane.YES_OPTION);
+				if (dialogRes == JOptionPane.YES_OPTION) {
 					dispose();
-					LoginWindow login = new LoginWindow();
+					new LoginWindow();
 				}
 			}
 		});
-		
+
 		/**
 		 * Exit menu item Action listener
 		 */
-		
-		((UsersMenu)menuBar).getExitItem().addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-			}
-		});
+
+		((UsersMenu) menuBar).getExitItem().addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
 	}
 }
